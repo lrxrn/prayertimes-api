d121210cf3e4accf299d66880a2b21b1735b5181
lrxrn <meesum632@gmail.com>
2024-05-14 00:22:03 +0500

    an actual page + move endpoints to /api

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 process.env.TZ = 'Indian/Maldives';
 
 const express = require('express');
+const path = require('path');
 const app = express();
+const staticPath = path.join(__dirname, 'public'); 
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use(express.static(staticPath));
 
 // Register routes
 const healthRouter = require('./routes/health');
 const todayPrayerRouter = require('./routes/today');
 const nextPrayerRouter = require('./routes/next');
 
-app.use('/health', healthRouter);
-app.use('/today', todayPrayerRouter);
-app.use('/next', nextPrayerRouter)
+app.use('/api/health', healthRouter);
+app.use('/api/today', todayPrayerRouter);
+app.use('/api/next', nextPrayerRouter)
 
 
 app.listen(80, () => {
